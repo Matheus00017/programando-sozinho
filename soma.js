9a302faccc59e25acf00d4928d2c2fdbed29511e
Matheus00017 <vidalmatheus321@gmail.com>
2024-09-01 14:56:09 -0300

    soma

---
+++ soma.js
@@ -0,0 +1,12 @@
+var primeiroNumero = parseInt(prompt("Digite o primeiro número"));
+var segundoNumero = parseFloat(prompt("Digite o segundo número"));
+
+var soma = primeiroNumero + segundoNumero;
+var subtracao = primeiroNumero - segundoNumero;
+var multiplicacao = primeiroNumero * segundoNumero;
+var divisao = primeiroNumero / segundoNumero;
+
+console.log("o resultado da soma é " + soma);
+console.log("o resultado da subtração é " + subtracao);
+console.log("o resultado da multiplicação é " + multiplicacao);
+console.log("o resultado da divisão é " + divisao);
